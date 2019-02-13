50faf06f18f1a150ebb6fec71ffab0a76822dee3
hkmaken <47613617+hkmaken@users.noreply.github.com>
2019-02-14 03:13:03 +0500

    Add files via upload

---
+++ api/data/Appliance_Schema.js
@@ -0,0 +1,18 @@
+var mongoose= require('mongoose');
+var Appliance_Model= new mongoose.Schema({
+
+	ApplianceName:
+ {
+     type:String , required:true,  "default":' '
+ }
+
+},
+
+{
+
+    versionKey: false
+
+});
+
+mongoose.model("appliance",Appliance_Model,"Appliance");
+
